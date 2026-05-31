23afdd9e3f2026d968fa69b2f270071cb858fa1c
Darius <dariusraphael97@gmail.com>
2026-05-30 23:37:59 -0400

    feat(plan-5-f): mission-control adopts AppShell

--- src/app/mission-control/page.tsx
+++ src/app/mission-control/page.tsx
@@ -2,13 +2,14 @@
 // build doesn't try to prerender it with blank secrets (same pattern as the new surfaces).
 export const dynamic = "force-dynamic";
 
-import { CockpitShell } from "@/components/cockpit/cockpit-shell";
+import { AppShell } from "@/components/layout/app-shell";
+import { AppSidebar } from "@/components/layout/app-sidebar";
 import { TopicQueuePanel } from "@/components/cockpit/topic-queue-panel";
 import { TrendingPanel } from "@/components/cockpit/trending-panel";
 
 export default function MissionControlPage() {
   return (
-    <CockpitShell>
+    <AppShell bare sidebar={<AppSidebar />}>
       <div className="h-full flex flex-col lg:flex-row">
         <div className="flex-1 min-w-0 lg:basis-3/5 lg:border-r lg:border-subtle">
           <TopicQueuePanel />
@@ -17,6 +18,6 @@ export default function MissionControlPage() {
           <TrendingPanel />
         </div>
       </div>
-    </CockpitShell>
+    </AppShell>
   );
 }
